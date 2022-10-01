791c958744206675a3e71f910e3b9af939fed18a
Shubham Patil <imsp2596@gmail.com>
2022-10-01 10:48:02 +0530

    coding challenge 1

---
+++ codingChallenge1.js
@@ -0,0 +1,10 @@
+const  heightMark=95;
+const  weightMark=1.88;
+const  heightJohn=85;
+const  weightJohn=1.76;
+
+const  bmiMark=(weightMark/heightMark**2)*100000;
+const  bmiJohn=(weightJohn/heightJohn**2)*100000;
+
+const  markHigherBMI=bmiMark>bmiJohn;
+console.log(bmiMark,bmiJohn,markHigherBMI,`🤞🤞🤞`);
